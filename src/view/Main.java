ccbe68c300d723af1ab8248eafd9632a11e25a78
VonLanplace <firekstar@gmail.com>
2025-08-23 22:39:50 -0300

    Projeto Terminado com inclusão do menu Principal

--- src/view/Main.java
+++ src/view/Main.java
@@ -10,10 +10,36 @@ public class Main {
 		super();
 	}
 
+	/*
+	 * A Classe Main.java deve dar as opções de chamadas do método ip ou do método
+	 * ping com JOptionPane e, dependendo da escolha, instanciar a Classe
+	 * RedesController.java e chamar o método escolhido. A opção de finalizar a
+	 * aplicação também deve estar disponível.
+	 */
 	public static void main(String[] args) {
+		boolean loop = true;
 		RedesController RC = new RedesController();
 
-		JOptionPane.showMessageDialog(null, RC.ip());
-		JOptionPane.showMessageDialog(null, RC.ping());
+		while (loop) {
+			String[] options = { "Close", "Ip", "Ping" };
+			int opc = JOptionPane.showOptionDialog(null, "Controle de Redes", null, 0, 0, null, options, options[0]);
+			switch (opc) {
+			case 1:
+				JOptionPane.showMessageDialog(null, RC.ip());
+				break;
+
+			case 2:
+				JOptionPane.showMessageDialog(null, RC.ping());
+				break;
+
+			case 0:
+			case -1:
+				loop = false;
+				break;
+
+			default:
+				JOptionPane.showMessageDialog(null, "Unexpected value: " + opc);
+			}
+		}
 	}
 }
